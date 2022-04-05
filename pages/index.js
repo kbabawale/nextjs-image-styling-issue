06fde732d6c8e2281f6e2267ea9daf32339a1917
kolapo babawale <kolapobabawale@gmail.com>
2022-04-05 23:02:15 +0100

    css

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,14 @@ import HeroImg from '../assets/hero.png'
 export default function Home() {
   return (
     <div className={styles.pagewrapper}>
-      <section className={styles.authpages_section}>
-        <span className={styles.authpages_heroimg}>
-          <Image
-            src={HeroImg}
-            alt="an hero img"
-            layout="fill"
-          />
-        </span>
+      <section>
+        <Image
+          src={HeroImg}
+          alt="an hero img"
+        />
       </section>
 
-      <section className={styles.authpages_section}>
+      <section>
         <h1 className={styles.title}>Welcome to Next.js!</h1>
       </section>
     </div>
